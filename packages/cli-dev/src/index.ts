e6740c3f51a1b91fe3a516655f23c0b728cb6a45
Bao Tran <pbao.tran.unsw@gmail.com>
2026-02-27 15:06:10 +0700

    chore(cli): bump version to 0.7.9

--- packages/cli-dev/src/index.ts
+++ packages/cli-dev/src/index.ts
@@ -25,7 +25,7 @@ const program = Effect.gen(function* () {
 
   const cli = Command.run(command, {
     name: 'GREP AI',
-    version: 'v0.7.8',
+    version: 'v0.7.9',
   })
 
   yield* cli(process.argv)
